a5b978c01367918289ddb936ea966b591209ff37
Fernando <fernandopinto.has.89@gmail.com>
2024-11-04 11:29:46 +0000

    atual

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
+// Express setup
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// MongoDB Connection
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://Pele89:Javali@cluster0.8tlpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Use JSON in request body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Run app in port 8080
+var port = 8080;
+app.listen(port);
+console.log("App running in port " + port);
+
+// Enable CORS for all routes
+const cors = require('cors');
+app.use(cors());
+
+// API Routes
+var PersonRoutes = require('./routes/personRoutes');
+app.use('/api/persons',PersonRoutes);
+var DriverRoutes = require('./routes/driverRoutes');
+app.use('/api/drivers',DriverRoutes);
+var CarRoutes = require('./routes/carRoutes');
+app.use('/api/cars',CarRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).json({ message: 'Resource not found' });
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
